1d2c32d496665f36b369d5d9b931dde32f94ea00
Kartick Gupta <KG@Rapidos-MacBook-Pro.local>
2022-12-15 04:47:18 +0530

    Add. Added Game of Life

--- grid/grid.go
+++ grid/grid.go
@@ -1,7 +1,13 @@
 package grid
 
+import "fmt"
+
 type Grid struct {
 	grid [][]uint8
+}
+
+type cell struct {
+	x, y int
 }
 
 func InitializeGrid(cols, rows int) Grid {
@@ -9,7 +15,57 @@ func InitializeGrid(cols, rows int) Grid {
 	for i := range grid {
 		grid[i] = make([]uint8, cols)
 	}
+
 	return Grid{
 		grid: grid,
+	}
+}
+
+func InitializeSeedGrid(cols, rows int, seedCells []cell) Grid {
+	grid := InitializeGrid(cols, rows)
+	for _, cell := range seedCells {
+		grid.grid[cell.y][cell.x] = 1
+	}
+	return grid
+}
+
+func (grid Grid) Print() {
+	for _, i := range grid.grid {
+		fmt.Println(i)
+	}
+}
+
+func (grid Grid) Tick(seedGrid Grid) {
+	for y, _ := range grid.grid {
+		for x, _ := range grid.grid[y] {
+			grid.updateCellStatus(x, y, seedGrid)
+		}
+	}
+}
+
+func (grid Grid) updateCellStatus(x, y int, seedGrid Grid) {
+	activeNeighbours := 0
+	for c := -1; c <= 1; c++ {
+		for r := -1; r <= 1; r++ {
+			neighbourColumn := c + x
+			neighbourRow := r + y
+			if neighbourColumn >= 0 && neighbourRow >= 0 && neighbourColumn < len(seedGrid.grid) && neighbourRow < len(seedGrid.grid[0]) {
+				if seedGrid.grid[neighbourRow][neighbourColumn] == 1 {
+					activeNeighbours += 1
+				}
+			}
+		}
+	}
+	if seedGrid.grid[y][x] == 1 {
+		activeNeighbours -= 1
+	}
+	if activeNeighbours < 2 || activeNeighbours > 3 {
+		grid.grid[y][x] = 0
+	}
+	if activeNeighbours == 2 || activeNeighbours == 3 {
+		grid.grid[y][x] = seedGrid.grid[y][x]
+	}
+	if activeNeighbours == 3 {
+		grid.grid[y][x] = 1
 	}
 }
--- grid/grid_test.go
+++ grid/grid_test.go
@@ -14,4 +14,122 @@ func TestGrid(t *testing.T) {
 		}
 		assert.Equal(t, expectedResponse, InitializeGrid(3, 3).grid)
 	})
+
+	t.Run("should return grid of 4 x 4 with 2 alive cells", func(t *testing.T) {
+		expectedResponse := [][]uint8{
+			{0, 0, 0, 0},
+			{1, 1, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+		}
+		seedCells := []cell{
+			cell{
+				x: 1,
+				y: 1,
+			},
+			cell{
+				x: 0,
+				y: 1,
+			},
+		}
+		assert.Equal(t, expectedResponse, InitializeSeedGrid(4, 4, seedCells).grid)
+	})
+
+	t.Run("should return grid of 4 x 4 with toad pattern", func(t *testing.T) {
+		expectedResponse := [][]uint8{
+			{0, 0, 1, 0, 0},
+			{0, 1, 0, 0, 1},
+			{0, 1, 0, 0, 1},
+			{0, 0, 0, 1, 0},
+			{0, 0, 0, 0, 0},
+		}
+		seedCells := []cell{
+			cell{
+				x: 1,
+				y: 1,
+			},
+			cell{
+				x: 2,
+				y: 1,
+			},
+			cell{
+				x: 3,
+				y: 1,
+			},
+			cell{
+				x: 2,
+				y: 2,
+			},
+			cell{
+				x: 3,
+				y: 2,
+			},
+			cell{
+				x: 4,
+				y: 2,
+			},
+		}
+		grid := InitializeGrid(5, 5)
+		seedGrid := InitializeSeedGrid(5, 5, seedCells)
+		grid.Tick(seedGrid)
+		assert.Equal(t, expectedResponse, grid.grid)
+	})
+
+	t.Run("should return grid of 4x4 with block pattern", func(t *testing.T) {
+		expectedResponse := [][]uint8{
+			{0, 0, 0, 0},
+			{0, 1, 1, 0},
+			{0, 1, 1, 0},
+			{0, 0, 0, 0},
+		}
+		seedCells := []cell{
+			cell{
+				x: 1,
+				y: 1,
+			},
+			cell{
+				x: 2,
+				y: 1,
+			},
+			cell{
+				x: 1,
+				y: 2,
+			},
+			cell{
+				x: 2,
+				y: 2,
+			},
+		}
+		grid := InitializeGrid(4, 4)
+		seedGrid := InitializeSeedGrid(4, 4, seedCells)
+		grid.Tick(seedGrid)
+		assert.Equal(t, expectedResponse, grid.grid)
+	})
+
+	t.Run("should return grid of 4 x 4 with blinker pattern", func(t *testing.T) {
+		expectedResponse := [][]uint8{
+			{0, 1, 0, 0},
+			{0, 1, 0, 0},
+			{0, 1, 0, 0},
+			{0, 0, 0, 0},
+		}
+		seedCells := []cell{
+			cell{
+				x: 1,
+				y: 1,
+			},
+			cell{
+				x: 0,
+				y: 1,
+			},
+			cell{
+				x: 2,
+				y: 1,
+			},
+		}
+		grid := InitializeGrid(4, 4)
+		seedGrid := InitializeSeedGrid(4, 4, seedCells)
+		grid.Tick(seedGrid)
+		assert.Equal(t, expectedResponse, grid.grid)
+	})
 }
